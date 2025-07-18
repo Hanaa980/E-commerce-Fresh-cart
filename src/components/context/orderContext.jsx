0f434c3190d22f6b1db37cc6b39bfe610e2d7157
hanaa <hanaaragab288@gmail.com>
2025-07-18 21:39:18 +0300

    handle allorders path

--- src/components/context/orderContext.jsx
+++ src/components/context/orderContext.jsx
@@ -18,7 +18,7 @@ export default function OrderContextProvider(props) {
 
   async function onlineOrder(cartId, values) {
     let { data } = await axios.post(
-      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=http://localhost:5173/E-commerce-Fresh-cart/%23`,
+      `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartId}?url=https://hanaa980.github.io/E-commerce-Fresh-cart/%23`,
       { values },
       { headers }
     );
